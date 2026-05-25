0f8d81e69790455d2685b2a8d9963e6c7f3f71c4
osvaldosergiofs-dev <osvaldosergio.fs@gmail.com>
2026-05-25 09:06:26 -0300

    Convert portfolio to static Vue Vite appp

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
     // GitHub Pages project sites are served from /<repository-name>/.
     // If the repository is renamed, update this value to match it.
-    base: '/meu-portifolio/',
+    base: '/Portf-lio-OsvaldoS-rgio/',
     plugins: [vue()],
 })
